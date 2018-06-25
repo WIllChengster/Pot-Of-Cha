9aebbd12f3b7fa4c0ca79d6988ffc66d459697a6
Will Cheng <35405384+burninggun@users.noreply.github.com>
2018-06-24 23:12:07 -0700

    navbar font and transition

--- src/components/home.js
+++ src/components/home.js
@@ -19,21 +19,21 @@ export default () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa quis et voluptatem repellendus quaerat! Nulla temporibus dolor, excepturi quos, molestiae ut cum ratione saepe esse omnis ullam obcaecati veniam?</p>
                 </div>
             </div>
-            <div className="container-fluid">
+            <div className="container">
                 <div className="row">
                     <Link style={green} to="/menu" className="col-3 square">
                         <div>
-                            <h3 className="text-white text-center">Check Out Our Menu</h3>
+                            <h4 className="text-white text-center">Check Out Our Menu</h4>
                         </div>
                     </Link>
                     <Link style={kaki} to="/locations" className="col-3 square">
-                        <h3 className="text-white text-center" >Find a Location</h3>
+                        <h4 className="text-white text-center" >Find a Location</h4>
                     </Link>
                     <Link style={orange} to="social" className="col-3 square">
-                        <h3 className="text-white text-center">Follow Us!</h3>
+                        <h4 className="text-white text-center">Follow Us!</h4>
                     </Link>
                     <Link style={green} to="/contact-us" className="col-3 square">
-                        <h3 className="text-white text-center">Any Questions?</h3>
+                        <h4 className="text-white text-center">Any Questions?</h4>
                     </Link>
                 </div>
                 
