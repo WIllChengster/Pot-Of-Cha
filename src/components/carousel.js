d45ad99e2b13683082f56246f31d3e42908c1b45
Will Cheng <35405384+burninggun@users.noreply.github.com>
2018-06-25 01:01:33 -0700

    2 slider repeat

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -7,7 +7,23 @@ class Carousel extends Component{
         this.state={
             switch: true,
             timeElapsed: 0,
-        }
+            slide1: {
+                image: null,
+                style: {
+                    transform: "translateX(0%)",
+                    transition: null,
+                },
+
+            },
+            slide2:{
+                image: null,
+                style: {
+                    transform: "translateX(100%)",
+                    transition: "1.5s",
+                },
+            },
+        };
+
         this.timeUpdate = this.timeUpdate.bind(this);
     }
 
@@ -16,23 +32,65 @@ class Carousel extends Component{
     }
 
     timeUpdate(){
-        if(this.state.timeElapsed < 4){
+        if(this.state.timeElapsed < 6){
             this.setState({
                 timeElapsed: this.state.timeElapsed + 1,
             });
         } else{
+            this.switchContent()
             this.setState({
-                timeElapsed: 0
-            })
-        }
-
+                timeElapsed: 0,
+            });
+        };
     }
 
+    switchContent(){
+        if(this.state.switch){
+            this.setState({
+                slide1: {
+                    ...this.state.slide1,
+                    style: {
+                        transform: "translateX(100%)",
+                        transition: "1.5s",
+                        transitionDelay: "1.5s"
+                    }
+                },
+                slide2: {
+                    ...this.state.slide2,
+                    style: {
+                        transform: "translateX(0%)",
+                        transition: "1.5s",
+                        zIndex: 1,
+                    }
+                },
+                switch: false,
+            });
 
-
+        } else{
+            this.setState({
+                slide1: {
+                    ...this.state.slide1,
+                    style: {
+                        transform: "translateX(0%)",
+                        transition: "1.5s",
+                        zIndex: 1,
+                    }
+                },
+                slide2: {
+                    ...this.state.slide2,
+                    style: {
+                        transform: "translateX(100%)",
+                        transition: "1.5s",
+                        transitionDelay: "1.5s"
+                    }
+                },
+                switch: true,
+            });
+        }
+    }
 
     render(){
-        console.log(this.state.timeElapsed);
+        console.log(this.state);
 
         const example = (
             <div className="content" >
@@ -42,7 +100,12 @@ class Carousel extends Component{
 
         return(
             <div className="slider">
-                {example}
+                <div style={this.state.slide1.style} className="content" >
+                    <h1 className="text-center">Your picture here!</h1>
+                </div>
+                <div style={this.state.slide2.style} className="content2" >
+                    <h1 className="text-center">Your picture here!</h1>
+                </div>
 
             </div>
         )
